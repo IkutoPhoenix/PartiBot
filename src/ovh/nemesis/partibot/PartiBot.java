6bc050ae859b12b71d85cd4d5a8f7da6b61feca4
Prince Ikuto Phoenix <33163837+IkutoPhoenix@users.noreply.github.com>
2021-07-19 18:33:21 +0200

    modifs

--- src/ovh/nemesis/partibot/PartiBot.java
+++ src/ovh/nemesis/partibot/PartiBot.java
@@ -2,13 +2,17 @@ package ovh.nemesis.partibot;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 
 import javax.security.auth.login.LoginException;
+import java.text.DecimalFormat;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import static org.toilelibre.libe.curl.Curl.$;
+import static org.toilelibre.libe.curl.Curl.curl;
 
 public class PartiBot {
 
@@ -31,11 +35,24 @@ public class PartiBot {
                     //System.out.println(result);
                     String beta = "Beta-Testers : " + (result.split("<p id=\"next-user-info\">You'll be <span class=\"highlight\">#")[1].split("</span>")[0]);
 
-                    if (!jda.getGuildChannelById(863401615629221898L).getName().equalsIgnoreCase(beta)) {
-                        jda.getGuildChannelById(863401615629221898L).getManager().setName(beta).queue();
+                    if (!jda.getGuildChannelById(863491833678266428L).getName().equalsIgnoreCase(beta)) {
+                        jda.getGuildChannelById(863491833678266428L).getManager().setName(beta).queue();
                     }
 
+                    /*int number = Integer.parseInt($("https://raw.githubusercontent.com/IkutoPhoenix/PartiBot/master/id?token=AH5AUPLEXN3KWXVBC73ZAUDA6U36G").replaceAll("\n", ""));
 
+                    JSONObject res = new JSONObject($("https://api.nemesis.ovh/Particubes/githubmilestone.php?id=" + number));
+
+                    if (res.getString("state").equalsIgnoreCase("open")) {
+                        int nb = res.getInt("closed_issues");
+                        int total = nb + res.getInt("open_issues");
+
+                        DecimalFormat decimalFormat = new DecimalFormat("#%");
+                        beta = res.getString("title") + " - " + nb + "/" + total + " - " + decimalFormat.format(nb / ((float) total));
+                        if (!jda.getGuildChannelById(863401615629221898L).getName().equalsIgnoreCase(beta)) {
+                            jda.getGuildChannelById(863401615629221898L).getManager().setName(beta).queue();
+                        }
+                    }*/
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
