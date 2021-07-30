4213181f05c1ecc1ab19f0dc6b281c4706bd251f
IkutoPhoenix <33163837+IkutoPhoenix@users.noreply.github.com>
2021-07-30 11:35:36 +0200

    Revert "removed beta testers count"
    This reverts commit 032e2d1e438c18bc4f6f12ad20b1093f49eb7f2c.

--- src/main/java/ovh/nemesis/partibot/PartiBot.java
+++ src/main/java/ovh/nemesis/partibot/PartiBot.java
@@ -50,6 +50,27 @@ public class PartiBot extends ListenerAdapter {
             System.exit(4);
         }
 
+        final Runnable refresh = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String result = $("https://particubes.com");
+                    String beta = "Beta-Testers : " + (result.split("<p id=\"next-user-info\">You'll be <span class=\"highlight\">#")[1].split("</span>")[0]);
+
+                    if (!jda.getGuildChannelById(863491833678266428L).getName().equalsIgnoreCase(beta)) {
+                        jda.getGuildChannelById(863491833678266428L).getManager().setName(beta).queue();
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        final ScheduledExecutorService user = Executors.newScheduledThreadPool(1);
+        user.scheduleAtFixedRate(refresh, 0L, 5L, TimeUnit.MINUTES);
+
+
     }
 
 
